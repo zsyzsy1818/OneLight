639550fe55a633359154ca5ee7d36b49304b1826
zsyzsy1818 <zsyzsy1818@hotmail.com>
2022-02-23 10:57:16 +0800

    init

---
+++ app/src/main/java/com/zsyzsy1818/onelight/ExampleAppWidgetProvider.java
@@ -0,0 +1,35 @@
+package com.zsyzsy1818.onelight;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Created by 小兔宝贝，一鹿有你 on 2022/2/23.
+ */
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            // Intent intent = new Intent(context, ExampleActivity.class);
+            Intent intent = new Intent();
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button，其中appwidget_provider_layout为创建的xml布局文件名称
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
+            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
--- app/src/main/java/com/zsyzsy1818/onelight/MainActivity.java
+++ app/src/main/java/com/zsyzsy1818/onelight/MainActivity.java
@@ -2,13 +2,168 @@ package com.zsyzsy1818.onelight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+//    private ToggleButton toggleButton;
+    private ImageButton imageButton;
+    private boolean flash;
+
+
+    private CameraManager manager;
+    private android.hardware.Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        if (manager == null) {
+//            manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+//        }
+////        toggleButton = findViewById(R.id.toggleButton);
+////        toggleButton.setOnCheckedChangeListener(this);
+//        imageButton = findViewById(R.id.imageButton);
+//        imageButton.setOnClickListener(this);
+//
+//        imageButton.setImageResource(R.drawable.light_on);
+//        openFlash();
+//        flash=true;
+        Intent intent = new Intent(AppWidgetManager.ACTION_APPWIDGET_BIND);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, "0");
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_PROVIDER, "info.componentName");
+// This is the options bundle discussed above
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_OPTIONS, "options");
+        startActivityForResult(intent, 0);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (manager == null) {
+            manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        }
+//        toggleButton = findViewById(R.id.toggleButton);
+//        toggleButton.setOnCheckedChangeListener(this);
+        imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(this);
+
+        imageButton.setImageResource(R.drawable.light_on);
+        openFlash();
+        flash=true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageButton:
+                //关
+                if (flash == true) {
+                    imageButton.setImageResource(R.drawable.light_off);
+                    closeFlash();
+                    flash = false;
+                    Toast.makeText(this, "大可爱鹿宝贝", Toast.LENGTH_SHORT).show();
+                }
+                //开
+                else if (flash == false) {
+                    imageButton.setImageResource(R.drawable.light_on);
+                    openFlash();
+                    flash = true;
+                    Toast.makeText(this, "小可爱兔宝贝", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (b==true){
+            openFlash();
+//            compoundButton.setButtonDrawable(R.drawable.light_on);
+            compoundButton.setButtonDrawable(R.drawable.light_on);
+            Toast.makeText(this, "大可爱鹿宝贝", Toast.LENGTH_SHORT).show();
+        } else if (b == false) {
+            closeFlash();
+//            compoundButton.setButtonDrawable(R.drawable.light_off);
+            compoundButton.setButtonDrawable(R.drawable.light_off);
+            Toast.makeText(this, "小可爱兔宝贝", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private void openFlash() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (manager != null) {
+                    manager.setTorchMode("0", true);
+                }
+            } else {
+//                camera = android.hardware.Camera.open();
+//                android.hardware.Camera parameters=camera.getParameters();
+//                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+//                camera.setParameters(parameters);
+//                camera.startPreview();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeFlash() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                if (manager == null) {
+                    return;
+                }
+                manager.setTorchMode("0", false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            if (camera == null) {
+                return;
+            }
+            camera.stopPreview();
+            camera.release();
+        }
+    }
+
+    private CameraManager.TorchCallback torchCallback = new CameraManager.TorchCallback() {
+        @Override
+        public void onTorchModeChanged(String cameraId, boolean enabled) {
+            super.onTorchModeChanged(cameraId, enabled);
+            manager.unregisterTorchCallback(torchCallback);
+            if (!enabled) {
+                //手电筒状态关闭，执行开启
+                openFlash();
+            } else {
+                //手电筒状态开启，执行关闭
+                closeFlash();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (manager != null && torchCallback != null) {
+            manager.unregisterTorchCallback(torchCallback);
+        }
     }
 }
