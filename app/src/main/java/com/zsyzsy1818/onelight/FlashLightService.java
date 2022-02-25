df4fcba2b9205520867172205a7c1ac0c0a15b37
zsyzsy1818 <zsyzsy1818@hotmail.com>
2022-02-25 17:17:07 +0800

    最低SDK版本降至23，service服务放到外部类 TODO：手电筒Handler判断状态。NewAppWidget待完善使用手电筒工具类。NewAppWidget基本功能可以用了。

---
+++ app/src/main/java/com/zsyzsy1818/onelight/FlashLightService.java
@@ -0,0 +1,67 @@
+package com.zsyzsy1818.onelight;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by  小可爱兔宝贝 on 2022/2/25
+ * Mail:   zhaoshiyu900310@163.com
+ * Description:
+ */
+public class FlashLightService extends Service {
+    static int num = 0;
+    static String tag_action = "Widget.Button.Click";
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        ComponentName thisWidget = new ComponentName(this, NewAppWidget.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(this);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.new_app_widget);
+
+        //点击按钮时
+        if (intent.getAction() != null) {
+            if (intent.getAction().equals(tag_action)) {
+                FlashLightUtils.getInstance().flashLightChange(MyApplication.context);
+                Log.d("TAG", "点击了appwidget: imageButton");
+                if (num % 2 == 1) {
+                    remoteViews.setImageViewResource(R.id.widget_button,R.drawable.xiaokeai);
+                }else
+                    remoteViews.setImageViewResource(R.id.widget_button,R.drawable.dakeai);
+                num++;
+            }
+        }
+        //定义一个Intent来发送按钮Action
+        Intent intent1 = new Intent();
+        intent1.setAction(tag_action);
+
+        // 用Intent实例化一个PendingIntent
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+        manager.updateAppWidget(thisWidget, remoteViews);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        num=0;
+        Log.d("TAG", "MyService被销毁了: ");
+        Intent service = new Intent(this, FlashLightService.class);
+        this.startService(service);
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/zsyzsy1818/onelight/NewAppWidget.java
+++ app/src/main/java/com/zsyzsy1818/onelight/NewAppWidget.java
@@ -1,25 +1,10 @@
 package com.zsyzsy1818.onelight;
 
-import android.app.PendingIntent;
-import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.hardware.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.os.IBinder;
-import android.util.Log;
-import android.view.View;
 import android.widget.RemoteViews;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.UiThread;
-
-import java.util.Objects;
 
 /**
  * Implementation of App Widget functionality.
@@ -30,8 +15,6 @@ import java.util.Objects;
  * Description:
  */
 public class NewAppWidget extends AppWidgetProvider {
-    static int num = 0;
-    static String tag_action = "Widget.Button.Click";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -39,7 +22,7 @@ public class NewAppWidget extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews   views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
 
-        Intent intent = new Intent(context, MyService.class);
+        Intent intent = new Intent(context, FlashLightService.class);
         context.startService(intent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -64,51 +47,5 @@ public class NewAppWidget extends AppWidgetProvider {
         FlashLightUtils.getInstance().flashLightDisabled();
     }
 
-    public static class MyService extends Service {
-
-        @Override
-        public int onStartCommand(Intent intent, int flags, int startId) {
-            ComponentName thisWidget = new ComponentName(this, NewAppWidget.class);
-            AppWidgetManager manager = AppWidgetManager.getInstance(this);
-            RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.new_app_widget);
-
-            //点击按钮时
-            if (intent.getAction() != null) {
-                if (intent.getAction().equals(tag_action)) {
-                    FlashLightUtils.getInstance().flashLightChange(MyApplication.context);
-                    Log.d("TAG", "点击了appwidget: imageButton");
-                    if (num % 2 == 1) {
-                        remoteViews.setImageViewResource(R.id.widget_button,R.drawable.xiaokeai);
-                    }else
-                    remoteViews.setImageViewResource(R.id.widget_button,R.drawable.dakeai);
-                    num++;
-                }
-            }
-            //定义一个Intent来发送按钮Action
-            Intent intent1 = new Intent();
-            intent1.setAction(tag_action);
-
-            // 用Intent实例化一个PendingIntent
-            PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent1, 0);
-            remoteViews.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
-            manager.updateAppWidget(thisWidget, remoteViews);
-            return super.onStartCommand(intent, flags, startId);
-        }
-
-        @Nullable
-        @Override
-        public IBinder onBind(Intent intent) {
-            return null;
-        }
-
-        @Override
-        public void onDestroy() {
-            num=0;
-            Log.d("TAG", "MyService被销毁了: ");
-            Intent service = new Intent(this, MyService.class);
-            this.startService(service);
-            super.onDestroy();
-        }
-    }
 
 }
